8f9994c252fad1debc77f432772aaa7ae2ac710b
Aaron Glasenapp <glassbead@gmail.com>
2014-09-24 03:19:25 -0600

    all tests passing

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -42,7 +42,7 @@ class TicketsController < ApplicationController
     end
 
     @tickets.each do |ticket|
-      ticket.location = current_admin.event.name if current_admin.event
+      ticket.location = current_admin.event.name if current_admin && current_admin.event
       ticket.save
     end
 
--- spec/features/create_location_for_ticket_spec.rb
+++ spec/features/create_location_for_ticket_spec.rb
@@ -7,24 +7,24 @@ feature 'admin can set the location for an event' do
 
     click_link 'Start Event'
 
-    expect(page).to have_text 'What is the location for this event?'
-    fill_in 'Location', with: 'Fall Fest'
-    click_link 'Start'
+    expect(page).to have_text 'Start a New Event'
+    fill_in 'Name', with: 'Fall Fest'
+    click_button 'Start'
 
     expect(page).to have_text 'Buy a Raffle Ticket'
 
     stubbed_ticket = FactoryGirl.build_stubbed(:ticket)
     fill_out_ticket(stubbed_ticket)
-    click_link 'Buy Tickets'
+    click_button 'Buy Tickets'
 
     expect(Ticket.find_by(email: stubbed_ticket.email).location).to eq('Fall Fest')
 
-    click_link 'Buy more Tickets'
+    click_link 'Buy more tickets'
 
     stubbed_ticket_2 = FactoryGirl.build_stubbed(:ticket)
     fill_out_ticket(stubbed_ticket_2)
     select '5', from: 'Number of tickets'
-    click_link 'Buy Tickets'
+    click_button 'Buy Tickets'
 
     Ticket.where(email: stubbed_ticket_2.email).each do |t|
       expect(t.location).to eq('Fall Fest')
