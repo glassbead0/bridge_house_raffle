93740bb327a4e6357a4f20d3bc7e32e613f1e323
Aaron Glasenapp <glassbead@gmail.com>
2014-09-25 09:15:19 -0600

    fixed logout error

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Admins::SessionsController < Devise::SessionsController
 
   def destroy
+    current_admin.event.delete if current_admin.event
     current_admin.event = nil
-    current_admin.event.delete
     super
   end
 
